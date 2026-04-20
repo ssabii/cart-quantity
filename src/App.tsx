1a53f78052282d30f9b23cd03c03ae567ffe2651
ssabii <fhy1930@gmail.com>
2026-04-20 09:18:56 +0900

    refactor: 카트 목록 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import CartItem from './features/cart/CartItem';
-import { useCartActions, useCartItems } from './features/cart/useCart';
-import { useEffect } from 'react';
-import { initialCartItems } from './data/cart';
 import CartSummary from './features/cart/CartSummary';
+import CartList from './features/cart/CartList';
 
 import './App.css';
 
 function App() {
-  const cartItems = useCartItems();
-  const { setItems } = useCartActions();
-  const isCartEmpty = cartItems.length === 0;
-
-  useEffect(() => {
-    setItems(initialCartItems);
-  }, []);
-
   return (
     <div className="app">
       <h1>장바구니</h1>
 
       <div className="cart-container">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} id={item.id} />
-        ))}
-
-        {isCartEmpty && (
-          <div className="empty-cart">
-            장바구니가 비었어요. <br />
-            상품을 추가해보세요!
-          </div>
-        )}
+        <CartList />
 
         <CartSummary />
       </div>
